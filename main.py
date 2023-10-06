7144f83c6aa89898acf460fd3485a9f351f54472
Owen6 <owen.box2@gmail.com>
2023-10-06 01:33:11 -0700

    auxiliary functions and intial state that needs to be manipulated for all possible states

--- main.py
+++ main.py
@@ -38,16 +38,47 @@ def main():
 
     newBoard = Board()
     newBoard.loadBoard(gridSize)
-    #newBoard.setValue(1,1)
-    newBoard.printBoard()
+    #newBoard.printBoard()
     newBoard = loadExampleBoard(newBoard,vertices)
-    newBoard.printBoard()
+    #newBoard.printBoard()
+    possibleSolutions()
 
 def loadExampleBoard(tempBoard, vertices):
     for i in range(gridSize):
         for j in range(gridSize):
             tempBoard.setValue(i,j,vertices[i][j])
     return tempBoard
+
+def possibleSolutions():
+
+    rule = [2,2]
+    initialState = initialize(rule)
+    print(initialState)
+
+
+def initialize(rule):
+    filled = listSum(rule)+len(rule)-1
+    xNum = gridSize - filled
+    index = 0
+    itemAdded = False
+    initialState = []
+    for i in range(gridSize):
+        if(index < len(rule) and itemAdded == False):
+            initialState.append(rule[index])
+            index = index + 1
+            itemAdded = True
+        else:
+            initialState.append('X')
+            itemAdded = False
+    return initialState
+
+def listSum(l):
+    sum = 0
+    index = 0
+    while index < len(l):
+        sum = sum + l[index]
+        index = index + 1
+    return int(sum)
 
 if __name__ == "__main__":
     main()
