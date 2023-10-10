82cd2be7f7566f6394450a9d77be39ac3edb52a2
Owen6 <owen.box2@gmail.com>
2023-10-10 01:19:30 -0700

    Possible ordering of list but missing x values in between

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 import itertools
 import re
+from collections import Counter
 
 def main():
     rule = [2,3]
-    test = [1,2,4,5]
+    rule2 = [2,3]
+    #test = [1,2,4,5]
     sample = [2,3,'X','X','X','X','X']
-    combinations = list(itertools.combinations_with_replacement(sample, 7))
+    #combinations = list(itertools.combinations_with_replacement(sample, 7))
+    combinations = list(itertools.product(sample,sample,sample,sample,sample,sample,sample))
     validCombos = []
 
-    for i in range(len(combinations)):
-        if(ruleInList(rule,combinations[i])):
-            validCombos.append(combinations[i])
+    for i in combinations:
+        if ruleInList(rule,i):
+            #if not ruleInList(i, validCombos):
+            validCombos.append(i)
+    validCombos = list(set(validCombos))
+    #for i in range(len(combinations)):
+    #    if(ruleInList(rule,combinations[i])):
+    #        if(not ruleInList(combinations[i],validCombos)):
+    #            validCombos.append(combinations[i])
 
     print(validCombos)
+    #print(combinations)
     print(len(validCombos), len(combinations))
     #print(ruleInList(rule,sample))
 
@@ -21,7 +31,15 @@ def ruleInList(x,y):
     y_str = ' '.join(map(str, y))
     instances = re.findall(x_str, y_str)
 
-    return len(instances) > 0
+    temp = Counter(y)
+    temp2 = Counter(x)
+    #order = []
+    valid = 0
+    for i in range(len(x)):
+        if temp[x[i]] == temp2[x[i]]:
+            valid = valid + 1
+            #order.append(x[i])
+    return len(instances) > 0 and valid == len(x) #and x == order
 
 
 if __name__ == "__main__":
